e1560c7af04f9c0848cc1ce2c4595ace01e3f525
amcnulty <amcnulty88@swbell.net>
2017-11-19 14:21:50 -0800

    Home page and questions
    Started adding content to the home page.
    Started adding the survey questions.

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -2,5 +2,4 @@ if (window.attachEvent) {window.attachEvent('onload', load);}
 else if (window.addEventListener) {window.addEventListener('load', load, false);}
 else {document.addEventListener('load', load, false);}
 function load() {
-    alert("Hello Friend Finder Survey");
 }
